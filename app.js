03a8ac0cb7672ef3b3df94bab074c69a8ec4732d
Singh Mohan <mslogicmaster@gmail.com>
2021-10-02 09:09:15 +0530

    fixed lineheight issue

--- app.js
+++ app.js
@@ -25,7 +25,7 @@
     doc.querySelector('.sparkline-text').style.backgroundColor = backgroundColor
     doc.querySelector(
       '#moneyLine',
-    ).innerHTML = `${moneyLine}<div class="sparkline-date-info" id="dateString">${dateString}</div>`
+    ).innerHTML = `<h1>${moneyLine}</h1><div class="sparkline-date-info" id="dateString">${dateString}</div>`
   }
   /**
    * Draw graph line
